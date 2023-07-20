c0a0012b0c6ec4f52707f1d046484e6702cf375f
lihaoyu.francis <825091124@qq.com>
2023-07-20 23:03:40 +0800

    云调用会自带用户信息2

--- handler/foodMenu.go
+++ handler/foodMenu.go
@@ -26,7 +26,7 @@ func ListFoodMenu(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "绑定错误", err)
 		return
 	}
-	res.Data = req
+	res.Data = r.Header
 	msg, err := json.Marshal(res)
 	if err != nil {
 		fmt.Fprint(w, "内部错误", err)
